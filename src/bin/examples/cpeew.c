8deba74c597e4879d3fa5c8e612ff46ab1f841e4
Alastair Poole <mail@alastairpoole.com>
2021-11-22 11:59:24 +0000

    cpeew: do the green cpufreq border.

--- src/bin/examples/cpeew.c
+++ src/bin/examples/cpeew.c
@@ -10,14 +10,23 @@ cb_event_change(Enigmatic_Client *client, Snapshot *s, void *data)
 {
    Eina_List *l;
    Cpu_Core *core;
+   Eina_Bool have_freq = cores_frequency();
 
    int n = 0;
    EINA_LIST_FOREACH(s->cores, l, core)
      {
         Evas_Object *lb = eina_list_nth(_objects, n++);
-        Evas_Object *rec = evas_object_data_get(lb, "r");
+        Evas_Object *rec = evas_object_data_get(lb, "r2");
         int c = cpu_colormap[core->percent & 0xff];
         evas_object_color_set(rec, RVAL(c), GVAL(c), BVAL(c), AVAL(c));
+
+        if (have_freq)
+          {
+             c = freq_colormap[core->freq & 0xff];
+             rec = evas_object_data_get(lb, "r1");
+             evas_object_color_set(rec, RVAL(c), GVAL(c), BVAL(c), AVAL(c));
+          }
+
         elm_object_text_set(lb, eina_slstr_printf("%i%%", core->percent));
      }
 }
@@ -52,17 +61,25 @@ cb_event_change_init(Enigmatic_Client *client, Snapshot *s, void *data)
         else col++;
 
         rec = evas_object_rectangle_add(evas_object_evas_get(tb));
-        evas_object_size_hint_min_set(rec, ELM_SCALE_SIZE(64), ELM_SCALE_SIZE(64));
+        evas_object_size_hint_min_set(rec, ELM_SCALE_SIZE(65), ELM_SCALE_SIZE(65));
         elm_table_pack(tb, rec, col, row, 1, 1);
         evas_object_show(rec);
 
         lb = elm_label_add(tb);
         evas_object_size_hint_weight_set(lb, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
         evas_object_size_hint_align_set(lb, EVAS_HINT_FILL, EVAS_HINT_FILL);
+        evas_object_data_set(lb, "r1", rec);
+
+        rec = evas_object_rectangle_add(evas_object_evas_get(tb));
+        evas_object_size_hint_min_set(rec, ELM_SCALE_SIZE(64), ELM_SCALE_SIZE(64));
+        elm_table_pack(tb, rec, col, row, 1, 1);
+        evas_object_show(rec);
+        evas_object_data_set(lb, "r2", rec);
+
+        elm_object_text_set(lb, eina_slstr_printf("%i%%", core->percent));
         elm_table_pack(tb, lb, col, row, 1, 1);
         evas_object_show(lb);
 
-        evas_object_data_set(lb, "r", rec);
         _objects = eina_list_append(_objects, lb);
         i++;
      }
@@ -89,18 +106,19 @@ elm_main(int argc, char **argv)
    EINA_SAFETY_ON_NULL_RETURN_VAL(client, 1);
 
    elm_policy_set(ELM_POLICY_QUIT, ELM_POLICY_QUIT_LAST_WINDOW_CLOSED);
-   win = elm_win_util_standard_add("cpeew", NULL);
+   win = elm_win_util_standard_add("cpeew", "cpeew");
    evas_object_size_hint_weight_set(win, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_size_hint_align_set(win, EVAS_HINT_FILL, EVAS_HINT_FILL);
    elm_win_autodel_set(win, 1);
    evas_object_event_callback_add(win, EVAS_CALLBACK_DEL, cb_win_del, NULL);
-   
+
    bx = elm_box_add(win);
    evas_object_size_hint_weight_set(bx, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_size_hint_align_set(bx, EVAS_HINT_FILL, EVAS_HINT_FILL);
    evas_object_show(bx);
 
    _table = tb = elm_table_add(win);
+   elm_table_padding_set(tb, ELM_SCALE_SIZE(2), ELM_SCALE_SIZE(2));
    evas_object_size_hint_weight_set(tb, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_size_hint_align_set(tb, 0.5, 0.5);
 
